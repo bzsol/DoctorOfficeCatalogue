4539b4e91f552a1cf6b7b993eacc349d636ba8ce
bzsol <61059824+bzsol@users.noreply.github.com>
2021-04-10 18:53:29 +0200

    Project added - Assistant Panel

---
+++ Assistant/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Assistant
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            FirstnameTextBox.Text = "";
+            HISTextBox.Text = "";
+            SecondNameTextBox.Text = "";
+            HomeAddressTextBox.Text = "";
+            ComplaintTextBox.Text = "";
+
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("Would you like to close this application?", "Do you want to exit?", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result.Equals(MessageBoxResult.Yes))
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
