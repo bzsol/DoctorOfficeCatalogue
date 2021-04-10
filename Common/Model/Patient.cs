2f6dda449f6315b4706e8569bab6ce1dc159a5a4
Koszta <koszta002@gmail.com>
2021-04-10 20:58:50 +0200

    Common project + model - validation classes

---
+++ Common/Model/Patient.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Model
+{
+    public class Patient
+    {
+        public long Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+
+        public string HomeAddress { get; set; }
+
+        public DateTime Intake { get; set; }
+
+        // Health Insurance Card -> TAJ
+        public string HIS { get; set; }
+
+        public string Complaint { get; set; }
+    }
+}
---
+++ Common/Validation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Common
+{
+    public class Validation
+    {
+        public static bool IsValidName(string firstname, string secondname)
+        {
+            Regex regex = new Regex(@"^[A-z]*$");
+            return regex.IsMatch(firstname) && regex.IsMatch(secondname);
+        }
+
+        public static bool IsValidHIS(string HIS)
+        {
+            Regex regex = new Regex(@"^[0-9]{3}\s[0-9]{3}\s[0-9]{3}$");
+            return regex.IsMatch(HIS);
+        }
+    }
+}
