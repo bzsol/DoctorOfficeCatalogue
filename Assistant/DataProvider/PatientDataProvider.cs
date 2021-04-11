d6fe65662641207956d3d191c6f5194688b55068
Koszta <koszta002@gmail.com>
2021-04-11 11:51:55 +0200

    patch

---
+++ Assistant/DataProvider/PatientDataProvider.cs
@@ -0,0 +1,51 @@
+﻿using Common.Model;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assistant.DataProvider
+{
+    public class PatientDataProvider
+    {
+        private const string server = "http://localhost:5000/api/patient";
+        public static IEnumerable<Patient> GetPatients()
+        {
+
+            using (var client = new HttpClient())
+            {
+                var respone = client.GetAsync(server).Result;
+
+                if (!respone.IsSuccessStatusCode)
+                {
+                    throw new InvalidOperationException(respone.StatusCode.ToString());
+                }
+                else
+                {
+                    var httpdata = client.GetStringAsync(server).Result;
+                    var patients = JsonConvert.DeserializeObject<IEnumerable<Patient>>(httpdata);
+                    return patients;
+                }
+
+            }
+        }
+
+        public static void CreatePatient(Patient patient)
+        {
+            using (var client = new HttpClient())
+            {
+                var httpdata = JsonConvert.SerializeObject(patient);
+                var httppacket = new StringContent(httpdata, Encoding.UTF8, "application/json");
+
+                var response = client.PostAsync(server, httppacket).Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new InvalidOperationException(response.StatusCode.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ Server/Controllers/PatientController.cs
@@ -0,0 +1,75 @@
+﻿using Common.Model;
+using Microsoft.AspNetCore.Mvc;
+using Server.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Controllers
+{
+
+    [ApiController]
+    [Route("api/patient")]
+    public class PatientController : Controller
+    {
+        // GET
+        [HttpGet]
+        public ActionResult<IEnumerable<Patient>> Get() {
+            return Ok(PatientRepo.GetPatients());
+        }
+
+        // POST
+        [HttpPost]
+        public ActionResult Post([FromBody] Patient patient) {
+            List<Patient> patients = PatientRepo.GetPatients().ToList();
+            patient.ID = patients.Count < 1 ? 1 : patients.OrderByDescending(e => e.ID).FirstOrDefault().ID + 1;
+            patients.Add(patient);
+            PatientRepo.SavePatients(patients);
+            return Ok();
+        }
+        // UPDATE
+        [HttpPut]
+        public ActionResult Put([FromBody] Patient patient)
+        {
+            var patients = PatientRepo.GetPatients().ToList();
+            var ChoosenOne = patients.FirstOrDefault(e => e.ID.Equals(patient.ID));
+            if (ChoosenOne.Equals(null))
+            {
+                return NotFound();
+            }
+            else
+            {
+
+                ChoosenOne.FirstName = patient.FirstName;
+                ChoosenOne.LastName = patient.LastName;
+                ChoosenOne.HomeAddress = patient.HomeAddress;
+                ChoosenOne.HIS = patient.HIS;
+                ChoosenOne.Intake = patient.Intake;
+
+                PatientRepo.SavePatients(patients);
+                return Ok();
+            }
+        }
+        // DELETE
+        [HttpDelete("{id}")]
+        public ActionResult Delete(long id)
+        {
+            var patients = PatientRepo.GetPatients().ToList();
+            var ChoosenOne = patients.FirstOrDefault(e => e.ID.Equals(id));
+            if (ChoosenOne.Equals(null))
+            {
+                return NotFound();
+            }
+            else
+            {
+                patients.Remove(ChoosenOne);
+                PatientRepo.SavePatients(patients);
+                return Ok();
+            }
+
+        }
+
+
+    }
+}
