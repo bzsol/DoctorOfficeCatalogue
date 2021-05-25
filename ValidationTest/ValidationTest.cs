8c20f07bc23b60ef112c2f654cc555ca121e98cd
Koszta <koszta002@gmail.com>
2021-05-25 16:20:27 +0200

    Using AAA convention on tests

--- ValidationTest/ValidationTest.cs
+++ ValidationTest/ValidationTest.cs
@@ -8,29 +8,86 @@ namespace ValidationTest
     [TestClass]
     public class ValidationTest
     {
-        [TestMethod]
-        public void IsValidHis()
+        [DataTestMethod]
+        [DataRow(true, "123 456 789")]
+        [DataRow(true, "222 222 222")]
+        [DataRow(true, "500 000 010")]
+        [DataRow(true, "999 999 999")]
+        [DataRow(true, "639 114 525")]
+        public void Fill_WithValidHIS_HISAccepted(bool expected, string HIS)
         {
-            Assert.AreEqual(true, Common.Validation.IsValidHIS("111 222 333"));
+            // Arrange
+            bool actual;
+
+            // Act
+            actual = Common.Validation.IsValidHIS(HIS);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod]
+        [DataRow(false, "44 555 777")]
+        [DataRow(false, "444 55 777")]
+        [DataRow(false, "444 555 77")]
+        [DataRow(false, "444555777")]
+        [DataRow(false, "444 555777")]
+        [DataRow(false, "444 555 77k")]
+        [DataRow(false, "-111 555 777")]
+        [DataRow(false, "444 5555 777")]
+        [DataRow(false, "444 0555 777")]
+        [DataRow(false, "444 555  777")]
+        public void Fill_WithValidHIS_HISDeclined(bool expected, string HIS)
+        {
+            // Arrange
+            bool actual;
+
+            // Act
+            actual = Common.Validation.IsValidHIS(HIS);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
         }
 
         [DataTestMethod]
         [DataRow(true, "Ka", "Pál")]
         [DataRow(true, "Józsi", "Ka")]
-        [DataRow(true, "Adolf", "Hitler")]
-        [DataRow(true, "Zombori", "Pál")]
-        [DataRow(true, "Kalányos", "Árpád")]
-        [DataRow(false, "k", "p")]
-        [DataRow(false, "K", "P")]
-        [DataRow(false, "kA", "lApál")]
-        [DataRow(false, "KaL", "Apál")]
-        [DataRow(false, "Kal", "ApáL")]
-        [DataRow(false, "Xae12", "Musk")]
-        [DataRow(false, "JóZ5ik4", "")]
-        [DataRow(false, "", "")]
-        public void IsValidName(bool expected, string firstName, string lastName)
+        [DataRow(true, "Kiss", "Bence")]
+        [DataRow(true, "Tisza", "István")]
+        [DataRow(true, "Nagy", "Gábor")]
+        public void Fill_WithValidName_NameAccepted(bool expected, string firstName, string lastName)
         {
-            Assert.AreEqual(expected, Common.Validation.IsValidName(firstName, lastName));
+            // Arrange
+            bool actual;
+
+            // Act
+            actual = Common.Validation.IsValidName(firstName, lastName);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod]
+        [DataRow(false, "", "")]
+        [DataRow(false, "K", "Áron")]
+        [DataRow(false, "Kiss", "Á")]
+        [DataRow(false, "KiSs", "Áron")]
+        [DataRow(false, "kiss", "Áron")]
+        [DataRow(false, "Kiss", "áron")]
+        [DataRow(false, "Kiss", "ÁroN")]
+        [DataRow(false, "XAE12", "Musk")]
+        [DataRow(false, "Xae12", "Musk")]
+        [DataRow(false, "Kis s", "Áron")]
+        public void Fill_WithUnvalidName_NameDeclined(bool expected, string firstName, string lastName)
+        {
+            // Arrange
+            bool actual;
+
+            // Act
+            actual = Common.Validation.IsValidName(firstName, lastName);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
